0b1e970bc09c04006164ba2e89a5b2d2de9de2a2
Sergey Semenov <lvfx00@gmail.com>
2018-09-24 00:23:41 +0700

    refactored code

--- raw_udp_client3/raw_udp_client.c
+++ raw_udp_client3/raw_udp_client.c
@@ -12,9 +12,9 @@
 #include <unistd.h>
 #include <linux/if.h>
 #include <linux/if_packet.h>
+#include <net/ethernet.h>
 
 #define SERVICE 52674
-#define UDP 17
 #define PORT_TO_USE 52675
 #define DATAGRAM_SIZE 4096
 #define MAX_IP_OUTPUT_LEN 100
@@ -29,7 +29,144 @@ unsigned short csum (unsigned short *buf, int nwords) {
   return ~sum;
 }
 
+// returns index of specified interface upon success
+// or -1 upon failure with appropirate errno setted up
+unsigned int get_index_of_iface(const char *iface_name) {
+  int ret;
+  int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+  if (-1 == sock) { 
+    return -1;
+  }
+
+  struct ifreq ifr;
+  strncpy(ifr.ifr_name, iface_name, IFNAMSIZ - 1); 
+  ret = ioctl(sock, SIOCGIFINDEX, &ifr);
+  if (-1 == ret) {
+    return -1;
+  }
+
+  ret = close(sock);
+  if (-1 == ret) {
+    return -1;
+  }
+  return ifr.ifr_ifindex;
+}
+
+// returns 0 and sets interface name to specified buffer upon success
+// or -1 upon failure with appropirate errno setted up
+// rough way...
+int get_default_iface_name(char *strbuf, unsigned int len) {
+  char ip_route_output[MAX_IP_OUTPUT_LEN];
+
+  FILE *cmd_ip_route = popen("ip route show default", "r");
+  if (NULL == cmd_ip_route) {
+    return -1;
+  }
+
+  fgets(ip_route_output, MAX_IP_OUTPUT_LEN, cmd_ip_route);
+  // got output string on the following format:
+  // default via X.Y.Z.T dev eth0  proto static
+  //                         ^^^^ need this
+  // skip first 4 words
+  char *interface_name = strtok(ip_route_output, " "); // " " is delimeter
+  for(int i = 0; i < 4; ++i) {
+  	interface_name = strtok(NULL, " ");
+  }
+  strncpy(strbuf, interface_name, len);
+
+  return 0;
+}
+
+// returns 0 and sets mac address to specified buffer upon success
+// or -1 upon failure with appropirate errno setted up
+// rough way...
+int get_default_gateway_mac(const char *iface_name, uint8_t buf[ETHER_ADDR_LEN]) {
+  char ip_neigh_output[MAX_IP_OUTPUT_LEN];
+  char ip_neigh_cmd[MAX_IP_INPUT_LEN];
+
+  snprintf(ip_neigh_cmd, MAX_IP_INPUT_LEN, "ip neigh show dev %s", iface_name);
+
+  FILE *cmd_ip_neigh = popen(ip_neigh_cmd, "r");
+  if (NULL == cmd_ip_neigh) {
+    return -1;
+  }
+
+  fgets(ip_neigh_output, MAX_IP_OUTPUT_LEN, cmd_ip_neigh);
+  // got output string on the following format:
+  // 192.168.1.1 lladdr 44:94:fc:8e:13:f0 REACHABLE
+  //                    ^^^^^^^^^^^^^^^^^ need this
+  // skip first 2 words
+  char *default_gateway_mac = strtok(ip_neigh_output, " "); // " " is delimeter
+  for(int i = 0; i < 2; ++i) {
+  	default_gateway_mac = strtok(NULL, " ");
+  }
+  
+  int values[ETHER_ADDR_LEN];
+  if (ETHER_ADDR_LEN != sscanf(default_gateway_mac, "%x:%x:%x:%x:%x:%x", 
+  &values[0], &values[1], &values[2], &values[3], &values[4], &values[5])) {
+    return -1;
+  }
+  for(int i = 0; i < ETHER_ADDR_LEN; ++i ) {
+    buf[i] = (uint8_t) values[i];
+  }
+
+  printf("Mac of default gateway: %.2X:%.2X:%.2X:%.2X:%.2X:%.2X\n" , buf[0], buf[1], buf[2], buf[3], buf[4], buf[5]);
+  return 0;
+}
+
+// returns 0 and sets mac address to specified buffer upon success
+// or -1 upon failure with appropirate errno setted up
+int get_iface_mac(const char *iface_name, uint8_t buf[ETHER_ADDR_LEN]) {
+  int ret;
+  int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+  if (-1 == sock) { 
+    return -1;
+  }
+
+  struct ifreq ifr;
+  strncpy(ifr.ifr_name, iface_name, IFNAMSIZ - 1);
+  ret = ioctl(sock, SIOCGIFHWADDR, &ifr);
+  if (-1 == ret) {
+    return -1;
+  }
+
+  memcpy(buf, ifr.ifr_hwaddr.sa_data, ETHER_ADDR_LEN);
+  printf("Mac of default interface: %.2X:%.2X:%.2X:%.2X:%.2X:%.2X\n", buf[0], buf[1], buf[2], buf[3], buf[4], buf[5]);
+
+  ret = close(sock);
+  if (-1 == ret) {
+    return -1;
+  }
+  return 0;
+}
+
+int get_iface_ipv4_addr(const char *iface_name, struct sockaddr_in *sa) {
+  int ret;
+  int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+  if (-1 == sock) { 
+    return -1;
+  }
+
+  struct ifreq ifr;
+  ifr.ifr_addr.sa_family = AF_INET;
+  strncpy(ifr.ifr_name, iface_name, IFNAMSIZ - 1); 
+
+  ret = ioctl(sock, SIOCGIFADDR, &ifr);
+  if (-1 == ret) {
+    return -1;
+  }
+
+  memcpy(sa, &ifr.ifr_addr, sizeof(struct sockaddr_in));
+  ret = close(sock);
+  if (-1 == ret) {
+    return -1;
+  }
+  return 0;
+}
+
 int main(int argc, char **argv) {
+  int ret;
+
   if (argc < 3 || strcmp(argv[1], "--help") == 0) {
     fprintf(stderr, "usage: %s server-host sending-string\n", argv[0]);
     exit(EXIT_FAILURE);
@@ -49,134 +186,101 @@ int main(int argc, char **argv) {
   struct sockaddr_in din;
   din.sin_family = AF_INET;
   din.sin_port = htons(SERVICE); // set network byte order
-  int ret = inet_pton(AF_INET, argv[1], &din.sin_addr);
-  if (ret == 0) {
+  ret = inet_pton(AF_INET, argv[1], &din.sin_addr);
+  if (0 == ret) {
     fprintf(stderr, "inet_pton - invalid address string\n");
     exit(EXIT_FAILURE);
   }
-
-  char ip_route_output[MAX_IP_OUTPUT_LEN];
-  // get default interface name
-  // rough way...
-  FILE *cmd_ip_route = popen("ip route show", "r");
-  fgets(ip_route_output, MAX_IP_OUTPUT_LEN, cmd_ip_route);
-  // got output string on the following format:
-  // default via X.Y.Z.T dev eth0  proto static
-  //                         ^^^^ need this
-  // skip first 4 words
-  char *interface_name = strtok(ip_route_output, " "); // " " is delimeter
-  for(int i = 0; i < 4; ++i) {
-  	interface_name = strtok(NULL, " ");
-  }
-
-  // get index number of default interface
-  // unsigned int index = if_nametoindex(interface_name);
-  // if (0 == index) {
-  //   perror("if_nametoindex");
-  //   exit(EXIT_FAILURE);
-  // }
-  struct ifreq ifr3;
-  strncpy(ifr3.ifr_name, interface_name, IFNAMSIZ - 1); 
-  if (ioctl(sockfd, SIOCGIFINDEX, &ifr3) == -1) {
-    perror("ioctl - SIOCGIFINDEX");
-    exit(EXIT_FAILURE);
-  }
-  unsigned int index = ifr3.ifr_ifindex;
-
-  // get MAC address of default gateway
-  // rough way...
-  char ip_neigh_output[MAX_IP_OUTPUT_LEN];
-  char ip_neigh_cmd[MAX_IP_INPUT_LEN];
-  snprintf(ip_neigh_cmd, MAX_IP_INPUT_LEN, "ip neigh show dev %s", interface_name);
-  FILE *cmd_ip_neigh = popen(ip_neigh_cmd, "r");
-  fgets(ip_neigh_output, MAX_IP_OUTPUT_LEN, cmd_ip_neigh);
-  // got output string on the following format:
-  // 192.168.1.1 lladdr 44:94:fc:8e:13:f0 REACHABLE
-  //                    ^^^^^^^^^^^^^^^^^ need this
-  // skip first 2 words
-  char *default_gateway_mac = strtok(ip_neigh_output, " "); // " " is delimeter
-  for(int i = 0; i < 2; ++i) {
-  	default_gateway_mac = strtok(NULL, " ");
-  }
-  unsigned char mac2[6];
-  if (6 != sscanf(default_gateway_mac, "%x:%x:%x:%x:%x:%x", &mac2[0], &mac2[1], &mac2[2], &mac2[3], &mac2[4], &mac2[5])) {
-    perror("sscanf - invalid default gateway mac address string");
-    exit(EXIT_FAILURE);
-  }
-  // printf("Mac of default gateway: %.2X:%.2X:%.2X:%.2X:%.2X:%.2X\n" , mac2[0], mac2[1], mac2[2], mac2[3], mac2[4], mac2[5]);
-
-  // get IPv4 address structure of default interface
-  struct ifreq ifr;
-  ifr.ifr_addr.sa_family = AF_INET;
-  strncpy(ifr.ifr_name, interface_name, IFNAMSIZ - 1); 
-  if (ioctl(sockfd, SIOCGIFADDR, &ifr) == -1) {
-    perror("ioctl - SIOCGIFADDR");
+  
+  char interface_name[MAX_IP_OUTPUT_LEN]; // TODO set more meaningful size constant
+  ret = get_default_iface_name(interface_name, MAX_IP_OUTPUT_LEN); 
+  if (-1 == ret) {
+    perror("get_default_iface_name");
     exit(EXIT_FAILURE);
   }
 
-  // get MAC address structure of default interface
-  struct ifreq ifr2;
-  strncpy(ifr2.ifr_name, interface_name, IFNAMSIZ - 1);
-  if (ioctl(sockfd, SIOCGIFHWADDR, &ifr2) == -1) {
-    perror("ioctl - SIOCGIFHWADDR");
+  struct sockaddr_in sin;
+  ret = get_iface_ipv4_addr(interface_name, &sin);
+  if (-1 == ret) {
+    perror("get_iface_ipv4_addr");
     exit(EXIT_FAILURE);
   }
-  // and save it to string
-  unsigned char *mac = (unsigned char *)ifr2.ifr_hwaddr.sa_data;
-  // printf("Mac of default interface: %.2X:%.2X:%.2X:%.2X:%.2X:%.2X\n" , mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+
+  // mac address of default interface
+  uint8_t src_mac[ETHER_ADDR_LEN];
+  ret = get_iface_mac(interface_name, src_mac);
+  if (-1 == ret) {
+    perror("get_iface_mac");
+    exit(EXIT_FAILURE);
+  }
+
+  // mac address of default gateway for default interface
+  uint8_t dest_mac[ETHER_ADDR_LEN];
+  ret = get_default_gateway_mac(interface_name, dest_mac);
+  if (-1 == ret) {
+    perror("get_default_gateway_mac");
+    exit(EXIT_FAILURE);
+  }
+
+  unsigned int iface_index = get_index_of_iface(interface_name);
+  if (-1 == iface_index) {
+    perror("get_inde_of_iface");
+    exit(EXIT_FAILURE);
+  }
 
   char datagram[DATAGRAM_SIZE];       // datagram buffer including udp and ip header
-  memset(datagram, 0, DATAGRAM_SIZE); /* zero out the buffer */
+  memset(datagram, 0, DATAGRAM_SIZE); // zero out the buffer
 
+  // init protocol headers and payload pointer
   struct ethhdr *ethh = (struct ethhdr *) datagram;
-  ethh->h_proto = htons(ETH_P_IP);
-  // fill Ethernet header
-  /// ethh->h_dest = ; // mac address of default gateway
-  memcpy(ethh->h_dest, mac2, ETH_ALEN);
-  // ethh->h_source = def_iface_mac; // mac address of default interface
-  memcpy(ethh->h_source, ifr2.ifr_hwaddr.sa_data, ETH_ALEN);
-
   struct ip *iph = (struct ip *) (datagram + sizeof(struct ethhdr));
+  struct udphdr *udph = (struct udphdr *)(datagram + sizeof(struct ethhdr) + sizeof(struct ip));
+  char *data = datagram + sizeof(struct ethhdr) + sizeof(struct ip) + sizeof(struct udphdr);
+
+  size_t udp_packet_size = sizeof(struct udphdr) + strlen(argv[2]);
+  size_t ip_packet_size = udp_packet_size + sizeof(struct ip);
+  size_t eth_packet_size = ip_packet_size + sizeof(struct ethhdr);
+
+  // fill payload
+  strcpy(data, argv[2]);
+
+  // fill UDP header
+  udph->source = htons(PORT_TO_USE);
+  udph->dest = htons(SERVICE);
+  udph->len = htons(udp_packet_size); // udp header + payload size
+  udph->check = csum((unsigned short *)udph, udp_packet_size >> 1);
+
   // fill IP header
   iph->ip_hl = 5; // header length in 32bit octes
   iph->ip_v = 4; // IP protocol version
   iph->ip_tos = 16; // type of service (controls priority)
-  iph->ip_len = htons(sizeof(struct ip) + sizeof(struct udphdr) + strlen(argv[2])); // packet size including ip and udp headers
+  iph->ip_len = htons(ip_packet_size);
   iph->ip_id = htons(54321); //id sequence number (for fragmentation, doesn't matter here)
   iph->ip_off = 0; // offset in datagram
   iph->ip_ttl = 64; // time to live
-  iph->ip_p = UDP; // transport layer protocol
-  iph->ip_sum = 0;		/* set it to 0 before computing the actual checksum later */
-  iph->ip_src.s_addr = ((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr.s_addr;
+  iph->ip_p = IPPROTO_UDP; // transport layer protocol
+  iph->ip_sum = csum((unsigned short *)iph, ip_packet_size >> 1); // >> 2 to get number of words (2 bytes)
+  iph->ip_src.s_addr = sin.sin_addr.s_addr;
   iph->ip_dst.s_addr = din.sin_addr.s_addr;
 
-  struct udphdr *udph = (struct udphdr *)(datagram + sizeof(struct ethhdr) + sizeof(struct ip)); // udp header pointer
-  // fill UDP header
-  udph->source = htons(PORT_TO_USE);
-  udph->dest = htons(SERVICE);
-  udph->len = htons(sizeof(struct udphdr) + strlen(argv[2])); // udp header + payload size
-  udph->check = 0; // leave zero so that kernel will fill the correct value
-
-  // fill payload
-  char *data = datagram + sizeof(struct ethhdr) + sizeof(struct ip) + sizeof(struct udphdr);
-  strcpy(data, argv[2]);
-
-  // fill checksum
-  iph->ip_sum = csum((unsigned short *)(datagram + sizeof(struct ethhdr)), iph->ip_len >> 1); // >> 2 to get number of words (2 bytes)
+  // fill Ethernet header
+  memcpy(ethh->h_source, src_mac, ETH_ALEN);
+  memcpy(ethh->h_dest, dest_mac, ETH_ALEN);
+  ethh->h_proto = ETH_P_IP;
 
   // init link layer address
   struct sockaddr_ll sa;
   memset(&sa, 0, sizeof(struct sockaddr_ll));
 
   sa.sll_family = PF_PACKET;
-  sa.sll_protocol = htons(ETH_P_IP);      
-  sa.sll_halen = 6; 
-  memcpy(sa.sll_addr, ifr2.ifr_hwaddr.sa_data, 6);
-  sa.sll_ifindex = index;
+  sa.sll_protocol = ETH_P_IP;      
+  sa.sll_halen = ETHER_ADDR_LEN; 
+  memcpy(sa.sll_addr, src_mac, ETHER_ADDR_LEN);
+  sa.sll_ifindex = iface_index;
   sa.sll_pkttype = PACKET_OUTGOING;
 
   // send data
-  if (sendto(sockfd, datagram, sizeof(struct ethhdr) + sizeof(struct ip) + sizeof(struct udphdr) + strlen(argv[2]), 0, (struct sockaddr *)&sa, sizeof(struct sockaddr_ll)) !=  sizeof(struct ethhdr) + sizeof(struct ip) + sizeof(struct udphdr) + strlen(argv[2])) {
+  if (sendto(sockfd, datagram, eth_packet_size, 0, (struct sockaddr *)&sa, sizeof(struct sockaddr_ll)) != eth_packet_size) {
     perror("sendto");
     exit(EXIT_FAILURE);
   }
